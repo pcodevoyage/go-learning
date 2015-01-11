98d07564dc33c57723da9cc251168415030836d9
pcodevoyage <pcodevoyage@gmail.com>
2015-01-11 00:48:37 +0000

    new method to read the file and return a map

--- todo.go
+++ todo.go
@@ -1,89 +1,108 @@
 package main
 
-import(
-  "os"
-  "bufio"
-  "fmt"
-  "strings"
-  "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
-func getFilenameFromConfig(){
+func getFilenameFromConfig() {
 
-  f,err := os.Open("config")
-  if err!=nil{
-    panic(err)
-  }
+	f, err := os.Open("config")
+	if err != nil {
+		panic(err)
+	}
 
-  defer f.Close()
+	defer f.Close()
 
-  bf := bufio.NewReader(f)
-  line, _,err :=bf.ReadLine()
+	bf := bufio.NewReader(f)
+	line, _, err := bf.ReadLine()
 
-  if err!=nil{
-    panic(err)
-  }
+	if err != nil {
+		panic(err)
+	}
 
-  //dbFile is a global variable.
-  dbFile =string(line)
+	//dbFile is a global variable.
+	dbFile = string(line)
 
-  //TODO : validate that dbFile is a valid file path.
+	//TODO : validate that dbFile is a valid file path.
 }
 
-func addTask(task string){
-  fmt.Println("ADD TASKS: file is "+dbFile+". Task :"+task)
+func addTask(task string) {
+	fmt.Println("ADD TASKS: file is " + dbFile + ". Task :" + task)
 }
 
-func showTask(){
-  f, err := os.Open(dbFile)
-  if err != nil {
-    panic(err)
-  }
-
-  defer f.Close()
-
-  bf := bufio.NewReader(f)
-  for{
-    line, isPrefix,err :=bf.ReadLine()
-
-    if err == io.EOF{
-      break
-    }
-
-    if err !=nil || isPrefix{
-      panic(err)
-    }
-
-    fmt.Println(string(line))
-  }
+func showTask() {
+	tasks := getAllTasks()
+	for key, value:= range tasks {
+		fmt.Print(key)
+		fmt.Println("> " + value)
+	}
 }
 
-func reOrderTask(oldNumber string, newNumber string){
-  fmt.Println("REORDER TASKS : file : "+dbFile+". Old :"+ oldNumber + ".New :"+newNumber)
+func getAllTasks() (tasks map[int64]string) {
+	f, err := os.Open(dbFile)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	tasks = make(map[int64]string)
+
+	bf := bufio.NewReader(f)
+	for {
+		line, isPrefix, err := bf.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil || isPrefix {
+			panic(err)
+		}
+
+		s := strings.Split(string(line), "|")
+		id, _ := strconv.ParseInt(s[0], 0, 64)
+		tasks[id] = s[1]
+	}
+	return
 }
 
-func removeTask(taskNum string){
-  fmt.Println("REMOVE TASKS : file : "+dbFile+" taskNum:"+taskNum)
+func reOrderTask(oldNumber string, newNumber string) {
+	fmt.Println("REORDER TASKS : file : " + dbFile + ". Old :" + oldNumber + ".New :" + newNumber)
+}
+
+func removeTask(taskNum string) {
+	fmt.Println("REMOVE TASKS : file : " + dbFile + " taskNum:" + taskNum)
 }
 
 var dbFile = ""
-func main(){
 
-  getFilenameFromConfig()
-  // fmt.Println(dbFile)
+func main() {
 
-  args := os.Args[1:]
+	getFilenameFromConfig()
+	// fmt.Println(dbFile)
 
-  if len(args) == 0 {
-    fmt.Println("Arguments\n-a Add a task \n-s Show all tasks\n-r remove task")
-    return
-  }
+	args := os.Args[1:]
 
-  switch args[0]{
-    case "-a": addTask(strings.Join(args[1:]," "))
-    case "-s": showTask()
-    case "-r": removeTask(args[1])
-    case "-c": reOrderTask(args[1],args[2])
-  }
+	if len(args) == 0 {
+		fmt.Println("Arguments\n-a Add a task \n-s Show all tasks\n-r remove task")
+		return
+	}
+
+	switch args[0] {
+	case "-a":
+		addTask(strings.Join(args[1:], " "))
+	case "-s":
+		showTask()
+	case "-r":
+		removeTask(args[1])
+	case "-c":
+		reOrderTask(args[1], args[2])
+	}
 
 }
